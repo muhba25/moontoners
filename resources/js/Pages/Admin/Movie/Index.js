97f0b3b6c0f54b9d333016fe1f9b5d0e6e332790
Baso <ibrahim@boer.co.id>
2022-09-26 15:52:49 +0700

    feat: admin update movie

---
+++ resources/js/Pages/Admin/Movie/Index.js
@@ -0,0 +1,60 @@
+import Button from "@/Components/Button";
+import FlashMessage from "@/Components/FlashMessage";
+import Authenticated from "@/Layouts/Authenticated/Index";
+import { Link } from "@inertiajs/inertia-react";
+
+export default function Index({ auth, flashMessage, movies }) {
+    return (
+        <Authenticated auth={auth}>
+            <Link href={route("admin.dashboard.movie.create")}>
+                <Button type="button" className="w-40 mb-8">
+                    Inset New Movie
+                </Button>
+            </Link>
+            {flashMessage?.message && (
+                <FlashMessage message={flashMessage.message} />
+            )}
+            <table className="table-fixed w-full text-center">
+                <thead>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Rating</th>
+                    <th colSpan={2}>Action</th>
+                </thead>
+                <tbody>
+                    {movies.map((movie) => (
+                        <tr key={movie.id}>
+                            <td>
+                                <img
+                                    src={`/storage/${movie.thumbnail}`}
+                                    className="w-32 rounded-md"
+                                />
+                            </td>
+                            <td>{movie.name}</td>
+                            <td>{movie.category}</td>
+                            <td>{movie.rating.toFixed(1)}</td>
+                            <td>
+                                <Link
+                                    href={route(
+                                        "admin.dashboard.movie.edit",
+                                        movie.id
+                                    )}
+                                >
+                                    <Button type="button" variant="warning">
+                                        Edit
+                                    </Button>
+                                </Link>
+                            </td>
+                            <td>
+                                <Button type="button" variant="danger">
+                                    Delete
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Authenticated>
+    );
+}
